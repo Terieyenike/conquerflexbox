9e0331910a999cd7d3515b469a175df51dc175cd
Oteri Eyenike <teyenike1@gmail.com>
2020-05-05 11:37:52 +0100

    fully responsive flexbox challenge

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector('.nav-toggle')
+const nav = document.querySelector('.nav')
+
+navToggle.addEventListener('click', handleNav)
+
+
+function handleNav() {
+  if(!navToggle.classList.contains('nav--visible')){
+    nav.classList.toggle('nav--visible');
+  }
+}
